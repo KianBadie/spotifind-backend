6b5742f0e141f08df0f0648015fd9f95e098a172
Kian Badie <kianbadie@gmail.com>
2022-11-16 08:35:16 -0800

    Remove cors as it is not necessary.

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const helmet = require('helmet');
 require('dotenv').config();
@@ -9,10 +8,6 @@ const authRouter = require('./routes/auth');
 const PORT = process.env.PORT || 8000;
 
 const app = express();
-
-// app.use(cors({
-//     origin: 'https://trulyfound.netlify.app'
-// }));
 
 app.use(helmet());
 app.use(cookieParser());
