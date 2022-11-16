7820abdc3cecd3ca52100615d2986d8acb5e5cb7
Kian Badie <kianbadie@gmail.com>
2022-11-16 08:27:08 -0800

    Remove unused index router.

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const cookieParser = require('cookie-parser');
 const helmet = require('helmet');
 require('dotenv').config();
 
-const indexRouter = require('./routes/index');
 const authRouter = require('./routes/auth');
 
 const PORT = process.env.PORT || 8000;
@@ -19,7 +18,6 @@ app.use(helmet());
 app.use(cookieParser());
 app.use(express.json());
 
-app.use('/', indexRouter);
 app.use('/auth', authRouter);
 
 app.listen(PORT, () => {
